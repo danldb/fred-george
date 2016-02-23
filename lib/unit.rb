eb406fe65feb6d708e6f5fe22de6f6f61532a69f
jafrog <howeveririna@gmail.com>
2016-02-23 11:54:17 +0000

    Adds unit table

--- lib/unit.rb
+++ lib/unit.rb
@@ -19,4 +19,9 @@ class Unit
 
   TEASPOON = Unit.new(1)
   TABLESPOON = Unit.new(3)
+  OUNCE = Unit.new(6)
+  CUP = Unit.new(48)
+  PINT = Unit.new(96)
+  QUART = Unit.new(192)
+  GALLON = Unit.new(768)
 end
